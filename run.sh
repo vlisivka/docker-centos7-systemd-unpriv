cb1373aaad726d70912e4ea3ebbad79511cc0dc5
Volodymyr M. Lisivka <vlisivka@gmail.com>
2016-02-13 02:53:57 +0200

    Added ability to supply additional arguments for docker in run.sh script.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run -d --stop-signal=$(kill -l RTMIN+3) -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name centos7_systemd vlisivka/centos7-systemd-unpriv
+docker run -d --stop-signal=$(kill -l RTMIN+3) -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name centos7_systemd vlisivka/centos7-systemd-unpriv "$@"
 sleep 1
 docker logs centos7_systemd
