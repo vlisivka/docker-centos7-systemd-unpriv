302381a8b0730ccd16b6cd48ad7c7080ce4684e8
Volodymyr M. Lisivka <vlisivka@gmail.com>
2016-02-25 14:10:18 +0200

    Check for PID 1 is added to init.sh.

--- files/usr/sbin/init.sh
+++ files/usr/sbin/init.sh
@@ -4,6 +4,11 @@ set -ue
 # Author: Volodymyr M. Lisivka <vlisivka@gmail.com>
 # License: GPLv2
 
+(( $$ == 1 )) || {
+  echo "ERROR: Script must be ran as PID 1, via 'CMD [\"/usr/sbin/init.sh\"]' entry in Dockerfile." >&2
+  exit 1
+}
+
 # Execute kickstart scripts, if any, before systemd is started to make final adjustments.
 # NOTE: For one time scripts, which must be ran once at start of the system,
 # write a "first boot" systemd service and then enable it from a Dockerfile or from
