7244a3cdc170d4c611020b8a396c53d25ab8acfe
Volodymyr M. Lisivka <vlisivka@gmail.com>
2016-02-25 17:56:45 +0200

    Name of container in build.sh/run.sh is fixed to match name at docker.io / quay.io.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -dt --stop-signal=$(kill -l RTMIN+3) -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name centos7_systemd vlisivka/centos7-systemd-unpriv "$@"
+docker run -dt --stop-signal=$(kill -l RTMIN+3) -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name centos7_systemd vlisivka/docker-centos7-systemd-unpriv "$@"
 echo "INFO: Use command \"docker logs centos7_systemd\" to see logs of container."
