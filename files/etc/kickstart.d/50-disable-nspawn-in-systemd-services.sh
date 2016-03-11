10b62f31bdebe4c324305de4945a03dd67d5074f
Volodymyr M. Lisivka <vlisivka@gmail.com>
2016-03-11 21:38:47 +0200

    Modification of systemd unit files in /usr/lib/systemd is replaced by overrides in /etc/systemd.

--- files/etc/kickstart.d/50-disable-nspawn-in-systemd-services.sh
+++ files/etc/kickstart.d/50-disable-nspawn-in-systemd-services.sh
@@ -2,4 +2,10 @@
 set -ue
 
 # Systemd cannot use NSPAWN in non-privileged container
-exec sed -i 's/PrivateTmp/#PrivateTmp/' /usr/lib/systemd/system/*.service
+for FILE in $(grep --files-with-matches '^PrivateTmp=' /usr/lib/systemd/system/*.service)
+do
+  SERVICE="$(basename "$FILE")"
+  OVERRIDE_DIR="/etc/systemd/$SERVICE.d"
+  mkdir -p "$OVERRIDE_DIR"
+  echo -e "[service]\nPrivateTmp=no" >"$OVERRIDE_DIR"/disable-private-tmp.conf
+done
--- files/etc/kickstart.d/50-disable-oomscoreadjust-in-systemd-services.sh
+++ files/etc/kickstart.d/50-disable-oomscoreadjust-in-systemd-services.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -ue
 
-# Systemd cannot adjust out-of-memory killer in non-privileged container
-exec sed -i 's/OOMScoreAdjust/#OOMScoreAdjust/' /usr/lib/systemd/system/*.service
+# Systemd cannot adjust score for kernel Out Of Memory Killer in non-privileged container
+
+for FILE in $(grep --files-with-matches '^OOMScoreAdjust=' /usr/lib/systemd/system/*.service)
+do
+  SERVICE="$(basename "$FILE")"
+  OVERRIDE_DIR="/etc/systemd/$SERVICE.d"
+  mkdir -p "$OVERRIDE_DIR"
+  echo -e "[service]\nOOMScoreAdjust=0" >"$OVERRIDE_DIR"/disable-oomscore-adjust.conf
+done
