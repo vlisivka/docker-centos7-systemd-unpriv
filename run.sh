2778ddb5fe87b92aa1543681c95f9aa26e3939d6
Volodymyr M. Lisivka <vlisivka@gmail.com>
2016-03-25 16:09:24 +0200

    Mount /sys/fs/cgroup submounts in ro too, except /sys/fs/cgroup/systemd. Hint of Daniel J Walsh.

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-docker run -dt --stop-signal=$(kill -l RTMIN+3) -v /sys/fs/cgroup:/sys/fs/cgroup:ro --name centos7_systemd vlisivka/docker-centos7-systemd-unpriv "$@"
+docker run -dt \
+  --stop-signal=$(kill -l RTMIN+3) \
+  -v=/sys/fs/cgroup:/sys/fs/cgroup:ro \
+  $(mount | awk '/\/sys\/fs\/cgroup\// && /systemd/ { print "-v="$3":"$3":rw" ; } /\/sys\/fs\/cgroup\// && ! /systemd/ { print "-v="$3":"$3":ro" ; }' ) \
+  --name centos7_systemd \
+  vlisivka/docker-centos7-systemd-unpriv \
+  "$@"
+
 echo "INFO: Use command \"docker logs centos7_systemd\" to see logs of container."
